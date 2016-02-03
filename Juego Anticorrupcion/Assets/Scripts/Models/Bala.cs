b401998ab3496bb656c28387e67d79d112867441
Jon Li <jonx_14@hotmail.com>
2016-02-03 16:43:47 -0500

    update scene
    agregados sprites 2d de prueba

--- Juego Anticorrupcion/Assets/Scripts/Models/Bala.cs
+++ Juego Anticorrupcion/Assets/Scripts/Models/Bala.cs
@@ -2,14 +2,20 @@
 
 public class Bala : MonoBehaviour
 {
+    public GameObject Explosion;
 	void OnCollisionEnter(Collision collision)
 	{
-		if (collision.transform.name.Contains ("Obstaculo")) 
+
+        GameObject Nuevaexp =  Instantiate(Explosion, transform.position, Quaternion.identity) as GameObject;
+        Destroy(Nuevaexp, 0.5f); 
+        if (collision.transform.name.Contains ("Obstaculo")) 
 		{
 			Destroy (collision.gameObject);
-		}
+        }
 
 		Destroy (this.gameObject);
 	}
+
+
 
 }
